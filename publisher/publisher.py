4445e0220e7a1f6dde6deda04f8c786eb3afd4bb
valemilanesio <valemilanesio@gmail.com>
2025-05-14 11:28:38 -0300

    modificados:     docker-compose.yml 	nuevos archivos: fluentbit/fluent-bit.conf 	nuevos archivos: fluentbit/parsers.conf 	modificados:     publisher/publisher.py

--- publisher/publisher.py
+++ publisher/publisher.py
@@ -1,24 +1,23 @@
 import psutil
 import paho.mqtt.client as mqtt
 import time
+import json
 
 BROKER = "localhost"
 PORT = 1883
-TOPICS = {
-    "cpu_temp": "sensors/cpu/temperature",
-    "cpu_percent": "sensors/cpu/usage_percent",
-    "mem_percent": "sensors/memory/usage_percent",
-    "net_sent": "sensors/network/bytes_sent",
-    "net_recv": "sensors/network/bytes_received",
-}
+TOPIC = "iot/sensors"
 
 client = mqtt.Client()
 client.connect(BROKER, PORT, 60)
 
 while True:
-    # Temperatura CPU (puede no estar disponible en todos los sistemas)
+    # Temperatura CPU (desde k10temp)
     temps = psutil.sensors_temperatures()
-    cpu_temp = temps.get("coretemp", [{}])[0].get("current", 0)
+    cpu_temp = 0
+    if "k10temp" in temps:
+        k10 = temps["k10temp"]
+        if k10 and isinstance(k10, list):
+            cpu_temp = k10[0].current
 
     # Uso de CPU y Memoria
     cpu_percent = psutil.cpu_percent(interval=1)
@@ -29,11 +28,16 @@ while True:
     net_sent = net.bytes_sent
     net_recv = net.bytes_recv
 
-    # Publicación MQTT
-    client.publish(TOPICS["cpu_temp"], cpu_temp)
-    client.publish(TOPICS["cpu_percent"], cpu_percent)
-    client.publish(TOPICS["mem_percent"], mem_percent)
-    client.publish(TOPICS["net_sent"], net_sent)
-    client.publish(TOPICS["net_recv"], net_recv)
+    # Mensaje JSON
+    payload = {
+        "cpu_temp": cpu_temp,
+        "cpu_percent": cpu_percent,
+        "mem_percent": mem_percent,
+        "net_sent": net_sent,
+        "net_recv": net_recv,
+    }
+
+    # Publicación en JSON
+    client.publish(TOPIC, json.dumps(payload))
 
     time.sleep(5)
